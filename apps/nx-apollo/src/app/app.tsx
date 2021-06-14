b66dc11617f522b73527d9b534592ba3551255e8
Pranay <pranaykgupta21@gmail.com>
2021-06-14 22:30:44 +0530

    connected graphql with react app

--- apps/nx-apollo/src/app/app.tsx
+++ apps/nx-apollo/src/app/app.tsx
@@ -1,23 +1,62 @@
-import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient, InMemoryCache,
-  NormalizedCacheObject, HttpLink} from '@apollo/client';
-import React from 'react';
-import './app.css';
+import { 
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  useMutation
+} from '@apollo/client';
+import React, { Component } from 'react';
+import './app.module.css';
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:3000/graphql',
-    headers: {
-      authorization: localStorage.getItem('token'),
-    },
-  }),
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache()
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <h1>My Lego Sets</h1>
-  </ApolloProvider>
-);
+const Students = gql`
+  query allSets {
+    allSets{
+      id,
+      name
+    }
+  }
+`;
+
+const AddStudent = gql`
+  mutation addSet {
+    addSet(name: "Poonam Sareen") {
+      id
+  }
+  }
+`;
+
+class App extends Component{
+  render(){
+    return(
+      <ApolloProvider client={client}>
+        <h1>Connecting React and GraphQl using apollo</h1>
+        <div className="flex">
+          <StudentsData />
+        </div>
+      </ApolloProvider>
+    )
+  }
+} 
+
+function StudentsData(){
+  const { loading, error, data } = useQuery(Students);
+  if(loading) return <p>Loading..</p>
+  if(error) return <p>Error...</p>
+  return data.allSets.map( ({id, name}) => (
+
+    <div key={id}>
+      <p>
+        {id} : {name}
+      </p>
+    </div>
+  ));
+}
+
 
 export default App;
